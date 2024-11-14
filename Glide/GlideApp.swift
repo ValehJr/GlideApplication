111892bb89db878a77d7f68a200a612dbcc2de39
ValehJr <i.valehjr@gmail.com>
2024-11-14 12:46:11 +0400

    Initial Commit

---
+++ Glide/GlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlideApp.swift
+//  Glide
+//
+//  Created by Valeh Ismayilov on 14.11.24.
+//
+
+import SwiftUI
+
+@main
+struct GlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
